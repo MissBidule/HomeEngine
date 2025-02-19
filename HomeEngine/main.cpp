08ac0ed735866fa45b88dde82652f56ee89304a9
Lilou Alidor <lilou.alidor@gmail.com>
2025-02-19 16:35:32 +0100

    Metal API added

--- HomeEngine/main.cpp
+++ HomeEngine/main.cpp
@@ -4,11 +4,14 @@
 //
 //  Created by MissBidule on 19/02/2025.
 //
+#include <Metal/Metal.hpp>
 
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
+    
+    MTL::Device* device = MTL::CreateSystemDefaultDevice();
+    
     std::cout << "Hello, World!\n";
     return 0;
 }
